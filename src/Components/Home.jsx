373bb556eb124aa42fc3f407a5d46cf5191b9f26
mohitaiits <mohit.kadwe.cs19@ggits.net>
2022-03-06 02:03:56 +0530

    code

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,44 +2,70 @@ import React, { useState } from 'react'
 import "../styles/style.css"
 import data from '../Data/data'
 import Landing from './Landing'
-function Card({img, name, desciption})
-{
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import { Button } from '@mui/material';
+
+
+function Card({ img, name, desciption, tags }) {
     return (
         <div className='card'>
             <div className='image'><img src={img} alt="" />
             </div>
             <h2>{name}</h2>
             <p>{desciption}</p>
-            <button className='primary-button'>Learn More</button>
+            <div className="tag_list">{tags.map((tag, index) => {
+                return (
+                    <p key={index} className="tag">
+                        <a href="#">
+                            {tag}
+                        </a>
+                    </p>
+                )
+            })}</div>
+            <div className="footer_card">
+                <div className="div_1">
+                    <a href="">
+                        <FavoriteBorderIcon />
+                    </a>
+                    <a href="#">
+                        <ChatBubbleOutlineIcon />
+                    </a>
+                </div>
+                <div className="div_2">
+                    <Button variant="outlined">Save</Button>
+                </div>
+            </div>
+            {/* <button className='primary-button'>Learn More</button> */}
         </div>
     )
 }
 function Home() {
     const [tag, settag] = useState('All');
-  return (
-    <div className='home_section'>
-        <Landing />
-        <div className="tabs">
-            <span className={tag ==='All'?'active':''} onClick={e=>settag('All')} >All</span>
-            <span className={tag ==='seasonable'?'active':''} onClick={e=>settag('seasonable')}>seasonable crops</span>
-            <span className={tag ==='suitable'?'active':''} onClick={e=>settag('suitable')}>suitable crops</span>
-            <span className={tag ==='seeds'?'active':''} onClick={e=>settag('seeds')}>seeds</span>
-            <span className={tag ==='tools'?'active':''} onClick={e=>settag('tools')}>farming tools and instruments</span>
+    return (
+        <div className='home_section'>
+            <Landing />
+            <div className="tabs">
+                <span className={tag === 'All' ? 'active' : ''} onClick={e => settag('All')} >All</span>
+                <span className={tag === 'seasonable' ? 'active' : ''} onClick={e => settag('seasonable')}>seasonable crops</span>
+                <span className={tag === 'suitable' ? 'active' : ''} onClick={e => settag('suitable')}>suitable crops</span>
+                <span className={tag === 'seeds' ? 'active' : ''} onClick={e => settag('seeds')}>seeds</span>
+                <span className={tag === 'tools' ? 'active' : ''} onClick={e => settag('tools')}>farming tools and instruments</span>
+            </div>
+            <div className="card_wrapper">
+                {
+                    (tag === 'All') ?
+                        data.map((item) => (<Card img={item.img} name={item.name} desciption={item.description} tags={item.tags} />))
+                        :
+                        data.map((item) => {
+                            if (item.tag === tag) {
+                                return (<Card img={item.img} name={item.name} desciption={item.description} tags={item.tags} />)
+                            }
+                        })
+                }
+            </div>
         </div>
-          <div className="card_wrapper">
-              {
-                  (tag === 'All') ?
-                      data.map((item) => (<Card img={item.img} name={item.name} desciption={item.description} />))
-                      :
-                      data.map((item)=> {
-                          if(item.tag === tag) {
-                              return (<Card img={item.img} name={item.name} desciption={item.description} />)
-                          }
-                      })
-              }
-          </div>
-    </div>
-  )
+    )
 }
 
 export default Home
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import LanguageIcon from '@mui/icons-material/Language'
 import "../styles/style.css"
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 function Navbar() {
   return (
     <div className="navbar">
-        <div className="left">
-            <h3>Kissania</h3>
-        </div>
-        <div className="middle">
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link href="">Products</Link>
-        </div>
-        <div className="right">
-            <button className='primary-button'><LanguageIcon /> Select Language </button>
-        </div>
+      <div className="left">
+        <h3>Kissania</h3>
+      </div>
+      <div className="middle">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link href="">Products</Link>
+      </div>
+      <div className="right">
+        <div style={{ marginRight: "15px" }} id="google_translate_element"></div>
+        {/* <button className='primary-button'><LanguageIcon /> Select Language </button> */}
+      </div>
     </div>
   )
 }
